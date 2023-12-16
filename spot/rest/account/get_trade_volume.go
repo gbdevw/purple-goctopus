184633fce06852734f911135a285972b88a61388
gbdevw <guillaume.braibant@lake42.eu>
2023-12-16 16:22:58 +0100

    accounts.get_trade_volume: OK

--- spot/rest/account/get_trade_volume.go
+++ spot/rest/account/get_trade_volume.go
@@ -1,49 +1,53 @@
 package account
 
-import "github.com/gbdevw/purple-goctopus/spot/rest/common"
+import (
+	"encoding/json"
+
+	"github.com/gbdevw/purple-goctopus/spot/rest/common"
+)
 
 // FeeTierInfo contains fee tier information.
 type FeeTierInfo struct {
 	// Current fee in percent
-	Fee string `json:"fee"`
+	Fee json.Number `json:"fee"`
 	// Minimum fee for pair if not fixed fee
-	MinimumFee string `json:"min_fee"`
+	MinimumFee json.Number `json:"minfee"`
 	// Maximum fee for pair if not fixed fee
-	MaximumFee string `json:"max_fee"`
+	MaximumFee json.Number `json:"maxfee"`
 	// Next tier's fee for pair if not fixed fee, empty if at lowest fee tier
-	NextFee string `json:"next_fee,omitempty"`
+	NextFee json.Number `json:"nextfee,omitempty"`
 	// Volume level of current tier (if not fixed fee. empty if at lowest fee tier)
-	TierVolume string `json:"tier_volume,omitempty"`
+	TierVolume json.Number `json:"tiervolume,omitempty"`
 	// Volume level of next tier (if not fixed fee. enpty if at lowest fee tier)
-	NextTierVolume string `json:"next_volume,omitempty"`
+	NextTierVolume json.Number `json:"nextvolume,omitempty"`
 }
 
-// GetTradeVolumeParameters contains Get Trade Volume required parameters.
-type GetTradeVolumeParameters struct {
-	// List of asset pairs to get fee info on
-	Pairs []string
-}
-
-// GetTradeVolumeOptions contains Get Trade Volume optional parameters.
-type GetTradeVolumeOptions struct {
-	// Whether or not to include fee info in results
-	FeeInfo bool
-}
-
-// Trade volume
-type TradeVolume struct {
+// GetTradeVolume result
+type GetTradeVolumeResult struct {
 	// Volume currency
 	Currency string `json:"currency"`
 	// Current discount volume
-	Volume string `json:"volume"`
+	Volume json.Number `json:"volume"`
 	// Fee info or Taker fee if asset is submitted to maker/taker fees - each key is an asset pair
 	Fees map[string]FeeTierInfo `json:"fees"`
 	// Maker fee info - each key is an asset pair
 	FeesMaker map[string]FeeTierInfo `json:"fees_maker"`
 }
 
-// GetTradeVolumeResponse contains Get Trade Volume response data.
+// GetTradeVolume request parameters.
+type GetTradeVolumeRequestParameters struct {
+	// List of asset pairs to get fee info on.
+	Pairs []string
+}
+
+// GetTradeVolume request options.
+type GetTradeVolumeRequestOptions struct {
+	// Whether or not to include fee info in results
+	FeeInfo bool
+}
+
+// GetTradeVolume response.
 type GetTradeVolumeResponse struct {
 	common.KrakenSpotRESTResponse
-	Result *TradeVolume `json:"result,omitempty"`
+	Result *GetTradeVolumeResult `json:"result,omitempty"`
 }
