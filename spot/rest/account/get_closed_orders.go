c3a9286a0e111063183d2c0fe4cf03bd632f0f6a
gbdevw <guillaume.braibant@lake42.eu>
2023-12-16 11:52:47 +0100

    add omitempty when rendering json

--- spot/rest/account/get_closed_orders.go
+++ spot/rest/account/get_closed_orders.go
@@ -19,27 +19,27 @@ type GetClosedOrdersOptions struct {
 	// Whether or not to include trades related to position in output.
 	//
 	// Defaults to false.
-	Trades bool `json:"trades"`
+	Trades bool `json:"trades,omitempty"`
 	// Restrict results to given user reference id.
 	//
 	// A nil value means no user reference will be provided.
-	UserReference *int64 `json:"userref"`
+	UserReference *int64 `json:"userref,omitempty"`
 	// Starting unix timestamp (seconds) or order tx ID of results (exclusive).
 	//
 	// A zero value means no filtering based on a start date.
-	Start int64 `json:"start"`
+	Start int64 `json:"start,omitempty"`
 	// Ending unix timestamp (seconds) or order tx ID of results (inclusive)
 	//
 	// A zero value means no filtering based on a end date.
-	End int64 `json:"end"`
+	End int64 `json:"end,omitempty"`
 	// Result offset for pagination
 	//
 	// A zero value means the first records will be fetched.
-	Offset int64 `json:"ofs"`
+	Offset int64 `json:"ofs,omitempty"`
 	// Which time to use to search.
 	//
 	// Defaults to "both" in case an empty string is provided. Cf. CloseTimeEnum.
-	Closetime string `json:"closetime"`
+	Closetime string `json:"closetime,omitempty"`
 	// Whether or not to consolidate trades by individual taker trades.
 	//
 	// Defaults to false.
@@ -49,7 +49,7 @@ type GetClosedOrdersOptions struct {
 // GetClosedOrders results.
 type GetClosedOrdersResult struct {
 	// Map where keys are transaction ID and values the related closed orders.
-	Closed map[string]OrderInfo `json:"closed"`
+	Closed map[string]OrderInfo `json:"closed,omitempty"`
 	// Amount of available order info matching criteria.
 	Count int `json:"count"`
 }
