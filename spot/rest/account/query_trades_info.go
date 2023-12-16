2a2fe699ef9284506b48cffc5a69c4d57c95b439
gbdevw <guillaume.braibant@lake42.eu>
2023-12-16 19:45:44 +0100

    query trades: OK

--- spot/rest/account/query_trades_info.go
+++ spot/rest/account/query_trades_info.go
@@ -2,20 +2,21 @@ package account
 
 import "github.com/gbdevw/purple-goctopus/spot/rest/common"
 
-// QueryTradesParameters contains Query Trades required parameters
-type QueryTradesParameters struct {
+// QueryTrades request parameters.
+type QueryTradesRequestParameters struct {
 	// List of transaction IDs to query info about (20 maximum).
-	TransactionIds []string
+	TransactionIds []string `json:"txid"`
 }
 
-// QueryTradesOptions contains Query Trades optional parameters.
-type QueryTradesOptions struct {
+// QueryTrades request options.
+type QueryTradesRequestOptions struct {
 	// Whether or not to include trades related to position in output.
+	//
 	// Defaults to false.
-	Trades bool
+	Trades bool `json:"trades"`
 }
 
-// QueryTradesInfoResponse contains Query Trades Info response data.
+// QueryTradesInfo response.
 type QueryTradesInfoResponse struct {
 	common.KrakenSpotRESTResponse
 	// Map where keys are transaction ID and values the requested trades.
