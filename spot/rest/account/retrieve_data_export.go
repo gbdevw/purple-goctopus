e9b164bd25b9f8bb4ec1babaa471b1fd47bfb2d3
gbdevw <guillaume.braibant@lake42.eu>
2023-12-20 21:00:58 +0100

    Use a ReadCloser to be able to close the stream

--- spot/rest/account/retrieve_data_export.go
+++ spot/rest/account/retrieve_data_export.go
@@ -10,6 +10,6 @@ type RetrieveDataExportParameters struct {
 
 // RetrieveDataExport response.
 type RetrieveDataExportResponse struct {
-	// Reader (tied to http.Response body) which can be used to download the zip archive which contains data.
-	Report io.Reader
+	// ReadCloser (tied to http.Response body) which can be used to download the zip archive which contains data.
+	Report io.ReadCloser
 }
