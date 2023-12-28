e070e9a0f8ebbc515d761e8c3863ed634bff7389
gbdevw <guillaume.braibant@lake42.eu>
2023-12-28 09:01:50 +0100

    Add helper functions for common tracing logic

--- spot/rest/tracing/tracing.go
+++ spot/rest/tracing/tracing.go
@@ -1,6 +1,11 @@
 package tracing
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/gbdevw/purple-goctopus/spot/rest/common"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -10,6 +15,10 @@ const (
 	PackageName = "goctopus_sdk_spot_rest"
 	// Package version
 	PackageVersion = "0.0.0"
+	// Span & events namespace
+	TracesNamespace = "goctopus.spot.rest"
+	// Kraken Sport REST API tracing header
+	ResponseTracingHeader = "x-trace-id"
 )
 
 // A helper function to trace an error (if any) and set the provided span status
@@ -33,5 +42,48 @@ func TraceErrorAndSetStatus(span trace.Span, err error) {
 		span.SetStatus(codes.Error, codes.Error.String())
 	} else {
 		span.SetStatus(codes.Ok, codes.Ok.String())
+	}
+}
+
+// # Description
+//
+// Helper function which handles the common tracing logic for the API operations outcome. The function will
+// set the provided span status depending the API operation outcome.
+//
+// # Inputs
+//
+//   - span: Span to populate
+//   - apiresp: Parsed API response. Can be nil and shoud be nil in case of error.
+//   - apiresp: Received HTTP response. Can be nil and might not be nil in case of error.
+//   - error: Error returned by the API operation. Can be nil in case of success.
+func TraceApiOperationAndSetStatus(span trace.Span, apiresp *common.KrakenSpotRESTResponse, httpresp *http.Response, err error) {
+	if httpresp != nil {
+		// Add synthetic tracing data about the received http response if any -> status code and tracing header
+		// This must not overlap too much with what an instrumented http.Client would report
+		span.AddEvent(TracesNamespace+".http.response", trace.WithAttributes(
+			attribute.Int("http.response.status_code", httpresp.StatusCode),
+			attribute.String("http.response.header."+ResponseTracingHeader, httpresp.Header.Get(ResponseTracingHeader)),
+		))
+	}
+	if err != nil {
+		// Record error if any and set span status to error
+		span.RecordError(err)
+		span.SetStatus(codes.Error, codes.Error.String())
+	} else {
+		// Check the API response
+		if apiresp != nil {
+			if len(apiresp.Error) > 0 {
+				// Records errors and set span to error if api response contains errors
+				span.RecordError(fmt.Errorf("received api response has errors: %v", apiresp.Error))
+				span.SetStatus(codes.Error, codes.Error.String())
+			} else {
+				// Set span status to ok if api response does not contain business errors
+				span.SetStatus(codes.Ok, codes.Ok.String())
+			}
+		} else {
+			// Record an error if no response is available despite no error is reported.
+			span.RecordError(fmt.Errorf("no api response is available despite no error has occured"))
+			span.SetStatus(codes.Error, codes.Error.String())
+		}
 	}
 }
---
+++ spot/rest/tracing/tracing_test.go
@@ -0,0 +1,66 @@
+package tracing
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/gbdevw/purple-goctopus/spot/rest/common"
+	"github.com/stretchr/testify/suite"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+)
+
+/*************************************************************************************************/
+/* TRACING: UNIT TEST SUITE                                                                      */
+/*************************************************************************************************/
+
+// Unit test suite for Tracing.
+type TracingUnitTestSuite struct {
+	suite.Suite
+	// Tracer used to generate spans
+	tracer trace.Tracer
+}
+
+// Run unit test suite.
+func TestTracingUnitTestSuite(t *testing.T) {
+	// Get a tracer from global tracer provider.
+	suite.Run(t, &TracingUnitTestSuite{
+		tracer: otel.GetTracerProvider().Tracer(PackageName, trace.WithInstrumentationVersion(PackageVersion)),
+	})
+}
+
+/*************************************************************************************************/
+/* TRACING: UNIT TEST SUITE                                                                      */
+/*************************************************************************************************/
+
+// Test TraceErrorAndSetStatus.
+//
+// This test only ensures all paths work as expected.
+func (suite *TracingUnitTestSuite) TestTraceErrorAndSetStatus() {
+	// Start a span
+	_, span := suite.tracer.Start(context.Background(), "test", trace.WithSpanKind(trace.SpanKindInternal))
+	defer span.End()
+	// Test function with no error
+	TraceErrorAndSetStatus(span, nil)
+	// Test function with an error
+	TraceErrorAndSetStatus(span, fmt.Errorf("fail"))
+}
+
+// Test TraceApiOperationAndSetStatus.
+//
+// This test only ensures all paths work as expected.
+func (suite *TracingUnitTestSuite) TestTraceApiOperationAndSetStatus() {
+	// Start a span
+	_, span := suite.tracer.Start(context.Background(), "test", trace.WithSpanKind(trace.SpanKindInternal))
+	defer span.End()
+	// Test function with no error and non nil responses
+	TraceApiOperationAndSetStatus(span, new(common.KrakenSpotRESTResponse), new(http.Response), nil)
+	// Test function with no error, non nil responses and errors in api response
+	TraceApiOperationAndSetStatus(span, &common.KrakenSpotRESTResponse{Error: []string{"1", "2"}}, new(http.Response), nil)
+	// Test function with an error
+	TraceApiOperationAndSetStatus(span, nil, nil, fmt.Errorf("fail"))
+	// Test all nil case
+	TraceApiOperationAndSetStatus(span, nil, nil, nil)
+}
