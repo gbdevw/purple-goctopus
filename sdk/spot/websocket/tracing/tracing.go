d6faaa8019c8984b7c493f34201a7bac75c349b9
gbdevw <guillaume.braibant@lake42.eu>
2024-02-16 16:15:16 +0100

    Add tralog combined helper function

--- sdk/spot/websocket/tracing/tracing.go
+++ sdk/spot/websocket/tracing/tracing.go
@@ -1,13 +1,15 @@
 package tracing
 
 import (
+	"log"
+
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
 
 const (
 	// Package name used as instrumentation ID
-	PackageName = "goctopus_sdk_spot_websocket"
+	PackageName = "goctopus.sdk.spot.websocket"
 	// Package version
 	PackageVersion = "0.0.0"
 	// Span & events namespace
@@ -64,6 +66,38 @@ func HandleAndTraceError(span trace.Span, err error) error {
 		panic("provided span must not be nil.")
 	}
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, codes.Error.String())
+	} else {
+		span.SetStatus(codes.Ok, codes.Ok.String())
+	}
+	return err
+}
+
+// A helper function to trace and log an error (if any), set the provided span status
+// accordingly and return the input error.
+//
+// The function is meant to replace code blocks like this:
+//
+//	if err != nil {
+//		logger.Println(err)
+//		span.RecordError(err)
+//		span.SetStatus(codes.Error, codes.Error.String())
+//		return err
+//	}
+//	span.SetStatus(codes.Ok, codes.Ok.String())
+//	return nil
+//
+// By this:
+//
+// return HandleAndTraLogError(span, err)
+func HandleAndTraLogError(span trace.Span, logger *log.Logger, err error) error {
+	// Panic if provided span or logger is nil
+	if span == nil || logger == nil {
+		panic("provided span and logger must not be nil.")
+	}
+	if err != nil {
+		log.Println(err)
 		span.RecordError(err)
 		span.SetStatus(codes.Error, codes.Error.String())
 	} else {
