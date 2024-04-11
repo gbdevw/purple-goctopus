38053d5d86534c2bb5de3495df253169ee6a6005
gbdevw <guillaume.braibant@lake42.eu>
2024-04-11 12:22:06 +0200

    Refine error message

--- sdk/spot/websocket/kraken_spot_websocket_client.go
+++ sdk/spot/websocket/kraken_spot_websocket_client.go
@@ -2719,7 +2719,7 @@ func (client *krakenSpotWebsocketClient) OnMessage(
 	matches := messages.MatchMessageTypeRegex.FindStringSubmatch(string(msg))
 	if len(matches) != 5 {
 		// Call OnReadError - Not the expected number of matches
-		err := fmt.Errorf("failed to extract the message type from '%s'", string(msg))
+		err := fmt.Errorf("failed to extract the message type from '%s' - not the expected number of matches", string(msg))
 		tracing.HandleAndTraLogError(span, client.logger, err)
 		client.OnReadError(ctx, conn, readMutex, restart, exit, err)
 		return
